88e87ce9316d88ac66f88247d5b2547dd63a3b3d
kimhyunso <gustn9515@gmail.com>
2024-06-05 00:39:43 +0900

    그리디 알고리즘 체육복

---
+++ 체육복/Solution.py
@@ -0,0 +1,27 @@
+def solution(n, lost, reserve):
+    
+
+    for index in reserve:
+        uniform[index - 1] += 1
+
+    for index in lost:
+        uniform[index - 1] -= 1
+    
+    for index in lost:
+        if index - 2 >= 0 and uniform[index - 2] == 2:
+            uniform[index - 2] -= 1
+            uniform[index - 1] += 1
+        elif index <= len(uniform) - 1 and uniform[index] == 2:
+            uniform[index] -= 1
+            uniform[index - 1] += 1
+       
+    
+    answer = 0
+    for i in uniform:
+        if i >= 1:
+            answer += 1
+    
+    return answer
+
+result = solution(5, [4, 5], [3, 4])
+print(result)
